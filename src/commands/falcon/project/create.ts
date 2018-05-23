d585de87b67f7415dd5482087061541f82d629ae
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2018-05-23 14:25:29 -0700

    Modify files related to falcon:project:create

--- src/commands/falcon/project/create.ts
+++ src/commands/falcon/project/create.ts
@@ -1,8 +1,8 @@
-import {flags} from '@oclif/command';
-import {join} from 'path';
-import {core, SfdxCommand} from '@salesforce/command';
-import * as _ from 'lodash';
-
+import {flags}              from '@oclif/command';                  // Why?
+import {core}               from '@salesforce/command';             // Why?
+import {join}               from 'path';                            // Why?
+import YeomanCommandBase    from '../../../yeoman-command-base';    // Why?
+import * as _               from 'lodash';                          // Why?
 
 
 // Initialize Messages with the current plugin directory
@@ -15,12 +15,13 @@ const messages = core.Messages.loadMessages('sfdx-falcon', 'create');
 /**
 * ─────────────────────────────────────────────────────────────────────────────────────────────────┐
 * CLI Command: falcon:project:create
+* Extends YeomanCommandBase, which itself extends SfdxCommand
 * ─────────────────────────────────────────────────────────────────────────────────────────────────┘
 */
-export default class Create extends SfdxCommand {
-  //─────────────────────────────────────────────────────────────────────────────┐
+export default class Create extends YeomanCommandBase {
+  //───────────────────────────────────────────────────────────────────────────┐
   // Set command-level properties.
-  //─────────────────────────────────────────────────────────────────────────────┘
+  //───────────────────────────────────────────────────────────────────────────┘
   public static description = messages.getMessage('commandDescription');
   public static hidden      = false;
   public static examples    = [
@@ -29,52 +30,51 @@ export default class Create extends SfdxCommand {
     `$ sfdx falcon:project:create -n "My SFDX-Falcon Project" -s my_ns_prefix`
   ];
   
-  //─────────────────────────────────────────────────────────────────────────────┐
+  //───────────────────────────────────────────────────────────────────────────┐
   // Define the flags used by this command.
-  //─────────────────────────────────────────────────────────────────────────────┘
+  //───────────────────────────────────────────────────────────────────────────┘
   protected static flagsConfig = {
-    projectname:  flags.string({char: 'n', description: messages.getMessage('projectnameFlagDescription')}),
-    namespace:    flags.string({char: 's', description: messages.getMessage('namespaceFlagDescription')})
+    projectname:  flags.string({required: true, char: 'n', description: messages.getMessage('projectnameFlagDescription')}),
+    namespace:    flags.string({char: 's', description: messages.getMessage('namespaceFlagDescription')}),
+    outputdir:    flags.string({char: 'd', description: messages.getMessage('outputdirFlagDescription')})
   };
 
-  //─────────────────────────────────────────────────────────────────────────────┐
+  //───────────────────────────────────────────────────────────────────────────┐
   // Determine which core SFDX arguments/features are required by this command.
-  //─────────────────────────────────────────────────────────────────────────────┘
+  //───────────────────────────────────────────────────────────────────────────┘
   protected static requiresProject        = false;  // True if an SFDX Project workspace is REQUIRED.
   protected static requiresUsername       = false;  // True if an org username is REQUIRED.
   protected static requiresDevhubUsername = false;  // True if a hub org username is REQUIRED.
   protected static supportsUsername       = false;  // True if an org username is OPTIONAL.
   protected static supportsDevhubUsername = false;  // True if a hub org username is OPTIONAL.
 
-  //─────────────────────────────────────────────────────────────────────────────┐
+  //───────────────────────────────────────────────────────────────────────────┐
   // Implement the run() function (this is what actually powers the command)
-  //─────────────────────────────────────────────────────────────────────────────┘
+  //───────────────────────────────────────────────────────────────────────────┘
   public async run(): Promise<any> { // tslint:disable-line:no-any
 
-    // Set default values for flags that were not provided.
-    const projectname = this.flags.projectname  ||  '.';
-    const namespace   = this.flags.namespace    ||  'force-app';
+    // Grab values from flags.  Set defaults for optional flags not set by user.
+    const projectnameFlag = this.flags.projectname;
+    const namespaceFlag   = this.flags.namespace  ||  'force-app';
+    const outputdirFlag   = this.flags.outputdir  ||  '.';
 
-    // Determine the path
-    const projectPath = 'some kind of code to get the path';
+    // TODO: Need to add validation of input values before running the generator
+    //       At a minimum, we should check to see if the outputdirFlag is a valid
+    //       filesystem name (eg no illegal chars like *).
 
-    let pathToTemplate = require.resolve('sfdx-falcon-template');
-    this.ux.log(pathToTemplate);
+    // Make an async call to the base object's generate() funtion.
+    await super.generate('appx-project', {
+      projectname: projectnameFlag,
+      namespace: namespaceFlag,
+      outputdir: outputdirFlag,
+      options: []
+    });
 
-    // This is an example of how you can create strings with variables and output them.
-    let outputString = `Hello ${projectname}! This is your namespace: ${namespace}`;
+    // DEVTEST: This is an example of how you can create strings with variables and output them.
+    let outputString = `Hello ${projectnameFlag}! This is your namespace: ${namespaceFlag}`;
     this.ux.log(outputString);
 
-
-
-
-
-
-    // this is an example of throwing an error
-    //throw new core.SfdxError(messages.getMessage('errorGitNotInstalled'));
-
-
-    // Return an object to be displayed with --json
-    return { retVal: 'someting coming back', outputString };
+    // Return empty JSON.
+    return { };
   }
 }
