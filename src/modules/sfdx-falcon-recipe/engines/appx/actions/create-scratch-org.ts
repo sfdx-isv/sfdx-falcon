fcdc51bb12528dd36801f91b254d29c50d958ffe
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2018-08-09 10:00:46 -0700

    Refactored to maximize extension of base class and minimize custom code

--- src/modules/sfdx-falcon-recipe/engines/appx/actions/create-scratch-org.ts
+++ src/modules/sfdx-falcon-recipe/engines/appx/actions/create-scratch-org.ts
@@ -5,16 +5,28 @@
  * @author        Vivek M. Chawla <@VivekMChawla>
  * @version       1.0.0
  * @license       MIT
- * @requires      module:???
- * @summary       ???
- * @description   ???
+ * @summary       Exposes the CLI Command force:org:create
+ * @description   Creates a new scratch org when given an Alias and a scratch-def.json file.
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
-import {SfdxFalconDebug}        from '../../../../../modules/sfdx-falcon-debug';  // Why?
-import {AppxEngineAction}       from '../../appx/actions';                        // Why?
-import {AppxEngineActionResult} from '../../appx';                                // Why?
-import {AppxEngineActionType}   from '../../appx/';                               // Why?
-import {AppxEngineStepContext}  from '../../appx/';                               // Why?
+// Import External Modules
+import * as path                from  'path';                                     // Node's path library.
+
+// Import Local Modules
+import {SfdxFalconDebug}        from  '../../../../sfdx-falcon-debug';            // Why?
+import {executeSfdxCommand}     from  '../../../../sfdx-falcon-executors/sfdx';   // Why?
+import {waitASecond}            from  '../../../../sfdx-falcon-async';            // Why?
+
+// Import Internal Engine Modules
+import {AppxEngineAction}       from  '../../appx/actions';                       // Why?
+import {AppxEngineActionResult} from  '../../appx';                               // Why?
+import {AppxEngineActionType}   from  '../../appx/';                              // Why?
+import {AppxEngineStepContext}  from  '../../appx/';                              // Why?
+import {AppxEngineContext}      from  '../../appx/';                              // Why?
+
+// Set the File Local Debug Namespace
+const dbgNs = 'create-scratch-org-action';
+
 
 //─────────────────────────────────────────────────────────────────────────────────────────────────┐
 /**
@@ -25,31 +37,31 @@ import {AppxEngineStepContext}  from '../../appx/';                             
  * @version     1.0.0
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
-export class CreateScratchOrg extends AppxEngineAction {
-
-
+export class CreateScratchOrgAction extends AppxEngineAction {
 
   //───────────────────────────────────────────────────────────────────────────┐
   /**
-   * @constructs  CreateScratchOrg
-   * @param       {any} xxxx ???? 
-   * @param       {any} xxxx ???? 
+   * @constructs  CreateScratchOrgAction
+   * @param       {AppxEngineContext} engineContext ???? 
    * @description ???
    * @version     1.0.0
    * @public
    */
   //───────────────────────────────────────────────────────────────────────────┘
-  constructor(stepContext:AppxEngineStepContext, stepOptions:object={}) {
+  constructor(engineContext:AppxEngineContext) {
 
     // Call parent constructor.
-    super(stepContext, stepOptions);
+    super(engineContext);
 
     // Set the Action Type
-    this._actionType = AppxEngineActionType.SFDX_CLI_COMMAND
-
-    // Run Base Validation
-    this.validateActionConfig();
+    this.actionType       = AppxEngineActionType.SFDX_CLI_COMMAND
+    this.actionName       = 'Create Scratch Org';
+    this.clsDbgNs         = 'CreateScratchOrgAction';
+    this.successDelay     = 2;
+    this.errorDelay       = 2;
+    this.progressDelay    = 1000;
     
+    return;
   }
 
   //───────────────────────────────────────────────────────────────────────────┐
@@ -61,61 +73,48 @@ export class CreateScratchOrg extends AppxEngineAction {
    * @public @async
    */
   //───────────────────────────────────────────────────────────────────────────┘
-  public async execute():Promise<AppxEngineActionResult> {
+  public async execute(actionOptions:any={}):Promise<AppxEngineActionResult> {
 
-    return null;
+    // Validate Command Options
+    if (typeof actionOptions.scratchOrgAlias === 'undefined') throw new Error(`ERROR_MISSING_OPTION: 'scratchOrgAlias'`);
+    if (typeof actionOptions.scratchDefJson  === 'undefined') throw new Error(`ERROR_MISSING_OPTION: 'scratchDefJson'`);
 
+    // Set the progress, error, and success messages for this action execution.
+    this.progressMessage  = `Creating scratch org '${actionOptions.scratchOrgAlias}' using ${actionOptions.scratchDefJson} (this can take 3-10 minutes)`;
+    this.errorMessage     = `Failed to create scratch org using ${actionOptions.scratchDefJson}`;
+    this.successMessage   = `Scratch org '${actionOptions.scratchOrgAlias}' created successfully using ${actionOptions.scratchDefJson}`;
+
+    // Create an SFDX Command Definition object to specify which command the CLI will run.
+    this.sfdxCommandDef = {
+      command:      'force:org:create',
+      progressMsg:  this.progressMessage,
+      errorMsg:     this.errorMessage,
+      successMsg:   this.successMessage,
+      observer:     actionOptions.observer,
+      commandArgs:  [] as [string],
+      commandFlags: {
+        FLAG_TARGETDEVHUBUSERNAME:  this.engineContext.devHubAlias,
+        FLAG_DEFINITIONFILE:        path.join(this.engineContext.configPath, actionOptions.scratchDefJson),
+        FLAG_SETALIAS:              actionOptions.scratchOrgAlias,
+        FLAG_DURATIONDAYS:          30,
+        FLAG_WAIT:                  10,
+        FLAG_NONAMESPACE:           true,
+        FLAG_SETDEFAULTUSERNAME:    true,
+        FLAG_JSON:                  true,
+        FLAG_LOGLEVEL:              this.engineContext.logLevel
+      }
+    }
+    SfdxFalconDebug.obj(`FALCON_EXT:${dbgNs}`, this.sfdxCommandDef, `${this.clsDbgNs}:execute:sfdxCommandDef: `);
+
+    // Execute the SFDX Command using an SFDX Executor function.
+    await executeSfdxCommand(this.sfdxCommandDef)
+      .then(result => {this.onSuccess(result)})
+      .catch(error => {this.onError(error)});
+
+    // Wait some number of seconds to give the user a chance to see the final status message.
+    await waitASecond(this.successDelay);
+
+    // Return whatever was put together by the onSuccess() method.
+    return this.actionResult;
   }
-
-  //───────────────────────────────────────────────────────────────────────────┐
-  /**
-   * @method      onError
-   * @param       {any} xxxx ???? 
-   * @param       {any} xxxx ???? 
-   * @description ???
-   * @version     1.0.0
-   * @public
-   */
-  //───────────────────────────────────────────────────────────────────────────┘
-  protected onError(error:any):any {
-
-    // TODO: Add Implementation
-
-  }
-
-  //───────────────────────────────────────────────────────────────────────────┐
-  /**
-   * @method      onSuccess
-   * @param       {any} xxxx ???? 
-   * @param       {any} xxxx ???? 
-   * @description ???
-   * @version     1.0.0
-   * @public
-   */
-  //───────────────────────────────────────────────────────────────────────────┘
-  protected onSuccess(result:any):any {
-
-    // TODO: Add Implementation
-
-  }
-
-  //───────────────────────────────────────────────────────────────────────────┐
-  /**
-   * @method      renderError
-   * @param       {any} xxxx ???? 
-   * @param       {any} xxxx ???? 
-   * @description ???
-   * @version     1.0.0
-   * @public
-   */
-  //───────────────────────────────────────────────────────────────────────────┘
-  public renderError(isErrorDebugEnabled:boolean=false):void {
-
-    // Render (ie. "print to terminal") the "base" of the error.
-    this.renderBaseError(isErrorDebugEnabled);
-
-    // Add customizations based on the needs of the derived class.
-    console.log(`Rendered from the derived class`);
-  }
-
 }
