feb48948541fca244299c85ec64b5676c7c7d7ff
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2018-08-27 21:45:21 -0700

    Add alias validation and improve git remote URI validation

--- src/modules/sfdx-falcon-validators/core-validator.ts
+++ src/modules/sfdx-falcon-validators/core-validator.ts
@@ -13,22 +13,26 @@
 import  {SfdxFalconDebug}     from '../sfdx-falcon-debug';         // Provides specialized debugging capabilities.
 import  {isGitUriValid}       from './git-validator';
 
+// File Globals
+const standardAlias = /^[a-zA-Z0-9_-]+$/
+
 //─────────────────────────────────────────────────────────────────────────────────────────────────┐
 /**
  * @function    gitRemoteUri
  * @access      public
- * @param       {string}      gitRemoteUri 
- * @returns     {boolean}     True if gitRemoteUri is a valid Git Remote URI.
+ * @param       {string}  gitRemoteUri  Required. A URI for a Git Remote
+ * @param       {RegExp}  [acceptedProtocols] Optional. RegExp that matches only certain protocols.
+ * @returns     {boolean} True if gitRemoteUri is a valid Git Remote URI.
  * @version     1.0.0
  * @description Core validation function for ensuring well-formed Git Remote URIs.
  *              See https://git-scm.com/docs/git-clone for detailed rules.
  */
 //─────────────────────────────────────────────────────────────────────────────────────────────────┘
-export function validateGitRemoteUri(gitRemoteUri:string):boolean {
+export function validateGitRemoteUri(gitRemoteUri:string, acceptedProtocols?:RegExp):boolean {
   // Debug, but no input validation. The isGitUriValid function takes care of that.
   SfdxFalconDebug.obj('FALCON:core-validator', arguments, `validateGitRemoteUri:arguments: `);
   // Leverage helper function from git-helper.
-  return isGitUriValid(gitRemoteUri);
+  return isGitUriValid(gitRemoteUri, acceptedProtocols);
 }
 
 //─────────────────────────────────────────────────────────────────────────────────────────────────┐
@@ -57,7 +61,42 @@ export function validateLocalPath(pathString:string):boolean {
   return (! localPathValidationRegex.test(pathString));
 }
 
+//─────────────────────────────────────────────────────────────────────────────────────────────────┐
+/**
+ * @function    validateStandardAlias
+ * @param       {string}  alias
+ * @param       {number}  [maxLength]
+ * @returns     {boolean} FALSE if alias contains invalid chars or is too long (optionally).
+ * @description Ensures that the provided alias does not have any invalid characters.  Optionally,
+ *              can also check for maximum length.
+ * @version     1.0.0
+ * @public
+ */
+//─────────────────────────────────────────────────────────────────────────────────────────────────┘
+export function validateStandardAlias(alias:string, maxLength?:number):boolean {
+  // Begin with Input Debug & Validation
+  SfdxFalconDebug.obj('FALCON:core-validator', arguments, `validateStandardAlias:arguments: `);
+  if (typeof alias !== 'string') {
+    throw new TypeError('ERROR_UNEXPECTED_TYPE');
+  }
 
+  // Find out if the string matches the "standard alias" pattern.  
+  if (standardAlias.test(alias)) {
+    if (typeof maxLength === 'number') {
+      if (alias.length <= maxLength) {
+        // Tested with maxLength
+        return true;
+      }
+    }
+    else {
+      // Tested without maxLength
+      return true;
+    }
+  }
+
+  // Failed at least one test
+  return false;
+}
 
 
 
